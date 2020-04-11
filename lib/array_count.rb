baa1b56431e5d47028938bf5e8040a5134abc601
rosie w <rosie.wilt@gmail.com>
2020-04-11 19:06:32 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,21 @@
+# def count_strings(array)
+# counter = 0
+# total = 0
+# while counter < array.count do
+# if array[counter].class == String
+#   total += 1
+# end
+# counter += 1
+# end
+# total
+# end
+
 def count_strings(array)
-counter = 0
-total = 0
-while counter < array.count do
-if array[counter].class == String
+  total = 0
+array.count do |element|
+if element.class == String
   total += 1
 end
-counter += 1
 end
 total
 end
